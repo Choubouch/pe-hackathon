2a0111e4d5dabea844169cdefe4de27db8748c33
Esteban Daude <esteban.daude@etu.minesparis.psl.eu>
2023-10-31 16:44:11 +0100

    Tess projects

--- tess_projects.py
+++ tess_projects.py
@@ -20,10 +20,100 @@
 # ---
 
 # %% [markdown]
-# # un notebook vierge
+# # tess_projects
 #
 # sauvé en Python
 
 # %%
-def foo():
-    print("hello world")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import utility as util
+
+# %% [markdown]
+# On commence par importer les données. Ici ce sont les planètes qui ont été identifiées par le telescope TESS comme étant d'éventuelles exoplanètes, mais non prouvé
+# Ensuite on selectionne les colonnes avec des données pertinentes, et qui sont communes aux autres tables pour pouvoir comparer. L'idée est de pouvoir faire des tracés communs avec les tables qui continnent les exoplanètes prouvées pour voire si les exoplanètes éventuelles sont pertinentes
+
+# %%
+df = pd.read_csv('tess_project_candidates.csv', skiprows = 69)
+
+assoc = util.get_rename_assoc('tess_project_candidates.csv')
+df.rename(columns=assoc, inplace=True)
+df = df[['TESS Object of Interest','TESS Input Catalog ID','RA [sexagesimal]','Dec [sexagesimal]','Planet Orbital Period Value [days]','Planet Radius Value [R_Earth]','Planet Insolation Value [Earth flux]','Planet Equilibrium Temperature Value [K]','Stellar Distance [pc]','Stellar Effective Temperature Value [K]','Stellar Radius Value [R_Sun]']]
+df.set_index('TESS Object of Interest', inplace = True)
+df.head()
+
+# %%
+df.shape
+
+# %% [markdown]
+# On enlève les lignes où il y a des valeurs NaN
+
+# %%
+df.dropna(how = 'any', inplace = True)
+df.shape
+
+# %%
+df.head()
+
+# %% [markdown]
+# ### On commence par regarder grossièrement les données (notamment les moyennes)
+
+# %%
+df.describe()
+
+# %% [markdown]
+# ### On trace la répartition des températures d'équilibre des planètes
+
+# %%
+df['Planet Equilibrium Temperature Value [K]'].hist(bins = 100, legend = True)
+
+# %% [markdown]
+# ### Ici on test la répartition des rayons des planètes
+
+# %%
+df['Planet Radius Value [R_Earth]'].hist(bins = 100, legend = True)
+
+# %% [markdown]
+# On voit qu'il y a des valeurs très élevée qui dégradent l'affichage, on froce donc la fenetre
+
+# %%
+df['Planet Radius Value [R_Earth]'].hist(bins = 100, legend = True, range = [0,50])
+
+# %% [markdown]
+# ### Là on regarde le lien qui existe entre la temperature de la planète et la temperature de son étoile
+
+# %%
+df.plot.scatter(x = 'Stellar Effective Temperature Value [K]', y = 'Planet Equilibrium Temperature Value [K]')
+
+# %% [markdown]
+# ### Et là entre la période de révolution et le rayon de l'étoile
+
+# %%
+df.plot.scatter(y = 'Stellar Radius Value [R_Sun]', x = 'Planet Orbital Period Value [days]')
+
+# %% [markdown]
+# On voit on tracé un peu bizarre
+
+# %% [markdown]
+# ### On regarde alors le rayon de la planète en fonction de la periode
+
+# %%
+df.plot.scatter(x = 'Planet Orbital Period Value [days]', y = 'Planet Radius Value [R_Earth]')
+
+# %% [markdown]
+# ### Là on regarde la corrélation entre la temperature de la planète et l'insolation qu'elle reçoit
+
+# %%
+df.plot.scatter(x = 'Planet Insolation Value [Earth flux]', y = 'Planet Equilibrium Temperature Value [K]')
+
+# %% [markdown]
+# On voit une jolie loi qui semble se dessiner
+
+# %% [markdown]
+# ### On va regarder le lien entre rayon de la planète et temperature d'équilibre
+
+# %%
+df.plot.scatter(x = 'Planet Equilibrium Temperature Value [K]', y = 'Planet Radius Value [R_Earth]')
+
+# %%
